c5f2d5f8cced2ade1915e0eb74a7637da320a99a
tdowgiel <tdowgiel@automotest.com>
2014-05-11 17:00:53 +0200

    "To Expensive Algorithm (from button) Working"

--- async/get_from_ebay.py
+++ async/get_from_ebay.py
@@ -3,30 +3,40 @@ from ebaysdk.finding import Connection as finding
 #from ebaysdk import finding
 
 
-filters = []
-filters.append({'name': 'LocatedIn','value': 'GB'})
-filters.append({'name': 'MinPrice','value': '2000'})
-filters.append({'name': 'MaxPrice','value': '23000'})
-filters.append({'name': 'HideDuplicateItems','value': 'true'})
+#filters = []
+#filters.append({'name': 'LocatedIn','value': 'GB'})
+#filters.append({'name': 'MinPrice','value': '2000'})
+#filters.append({'name': 'MaxPrice','value': '23000'})
+#filters.append({'name': 'HideDuplicateItems','value': 'true'})
 
 sort_order = 'PricePlusShippingHighest'
+filters_s = []
 
 
-def get_from_ebay(fliters):	
+def get_from_ebay():	
 	try:
 		api = finding(siteid='EBAY-GB', appid="TomaszDo-ac84-4764-9f1f-c3d9de2f3e59") #prod server
 		#api = finding(domain='svcs.sandbox.ebay.com', appid="TomaszDo-10f2-4c33-a762-9d5056c2784a")
-		#api.execute('findItemsAdvanced', {'keywords': 'Python'})
 		api.execute('findItemsAdvanced', { 'keywords': 'Car',
                                                    'categoryId' : [ '9801' , '29751' , '9800' ],
-                                                  'itemFilter': filters , 'sortOrder': sort_order, })
+                                                  'itemFilter': filters_s , 'sortOrder': sort_order, })
 		return api.response_dict()
 	except Exception, e:
 		raise e
 
 
-def show_results():
-        x = get_from_ebay('nic')
+def show_results(min_price=1000, max_price=5000):
+#def show_results():
+        global filters_s 
+        filters_s = []
+        filters_s.append({'name': 'LocatedIn','value': 'GB'})
+        filters_s.append({'name': 'MinPrice','value': str(min_price) })
+        filters_s.append({'name': 'MaxPrice','value': str(max_price) })
+        filters_s.append({'name': 'HideDuplicateItems','value': 'true'})
+		
+        print filters_s
+		
+        x = get_from_ebay()
 
         print type(x)
 
@@ -41,8 +51,7 @@ def show_results():
         count = 3
         output = []
         for i in results:
-                #print i['title']
-                #print i
+
                 output.append(i)
                 count -= 1
                 if not count:
@@ -51,3 +60,6 @@ def show_results():
         return output
 
 
+def read_first():
+	print "read"
+	return "to expensive"
